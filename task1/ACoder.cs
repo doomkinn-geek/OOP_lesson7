a161eda7579b241ff0920762747b05a9160ce6dd
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-11-25 18:41:56 +0300

    Добавьте файлы проекта.

---
+++ task1/ACoder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1
+{
+    public class ACoder : ICoder
+    {
+        public string Decode(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encode(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ task1/ICoder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1
+{
+    public interface ICoder
+    {
+        public string Encode(string input);
+        public string Decode(string input);
+    }
+}
